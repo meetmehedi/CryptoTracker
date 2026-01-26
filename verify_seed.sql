7329b0a7136c7b89f238690d93f923e00a1a7a8d
meeetmehedi <meehdibafsj@gmail.com>
2026-01-27 04:28:45 +0600

    Final Clean Repo: Removed redundant templates and test scripts

--- verify_seed.sql
+++
@@ -1,9 +0,0 @@
-USE cppt_db;
-SELECT 'Counting Coins...' as check_name;
-SELECT COUNT(*) FROM Coins;
-SELECT 'Sample Coins...' as check_name;
-SELECT * FROM Coins LIMIT 5;
-SELECT 'Counting Prices...' as check_name;
-SELECT COUNT(*) FROM Prices;
-SELECT 'Sample Prices...' as check_name;
-SELECT * FROM Prices LIMIT 5;
